018f2895c413565aeffea64d6880d1e82a68d09f
kennethwi <23746270+kennethwi@users.noreply.github.com>
2022-10-31 08:55:57 +0100

    Lade till tjänare

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. SKRIVER HÄR ABC -
-          1234 - HEJ - KANEL
+          1234 - HEJ - KANEL - Tjänare
         </p>
         <a
           className="App-link"
